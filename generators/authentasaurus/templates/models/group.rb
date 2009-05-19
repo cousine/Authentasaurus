d9ec8e2ae237b6c02aa51925007d7891bd7a2441
Omar Mekky <alfaengine@hotmail.com>
2009-05-20 01:51:45 +0300

    fixed small associativity in group model

--- generators/authentasaurus/templates/models/group.rb
+++ generators/authentasaurus/templates/models/group.rb
@@ -1,5 +1,5 @@
 class Group < ActiveRecord::Base
-  has_many :<%= class_name.downcase %>, :dependent => :destroy
+  has_many :<%= class_name.downcase.pluralize %>, :dependent => :destroy
   has_many :permissions, :dependent => :destroy
   has_many :areas, :through => :permissions
 
